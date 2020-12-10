97cf3eb1c3930a49f455ee9688e7727c486e7807
itish-agarwal <itishagarwal2000@gmail.com>
2020-12-10 18:31:18 +0530

    Add the functionality of adding contact (without styling)

--- index.js
+++ index.js
@@ -8,15 +8,50 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//middleware - parse -> to decode the data from the form
+app.use(express.urlencoded());
+
+var contactList = [
+    {
+        name: "Tom",
+        phone: "123456789"
+    },
+    {
+        name: "Jerry",
+        phone: "987654321"
+    }, 
+    {
+        name: "Dog",
+        phone: "3432546436"
+    }
+];
 
 app.get('/', function(req, res) {
-    return res.render('home', {title: "My Contacts List"});
+    return res.render('home', {
+        title: "My Contacts List",
+        contacts_list: contactList
+    });
 });
 
 app.get('/practise', function(req, res) {
     return res.render('practise', {
         title: "Let us practise with EJS"
     });
+});
+
+//get form data
+app.post('/create-contact', function(req, res) {
+    
+    //data is encoded as a string when it is sent from the browser
+    //we need to decode it to be able to use it in the form of an object
+    
+    //hence we need a parser to read the data
+    //this parser creates a property of the object 'req' as 'body' and puts the data as properties of 'body'
+    // console.log(req.body);
+
+    contactList.push(req.body);
+    return res.redirect('/');
+
 });
 
 app.listen(port, function(err) {
